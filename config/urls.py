92682d03791abf4361e5a7fca404af2a2924d52a
Douglas <douglassantos99249@gmail.com>
2024-10-16 21:33:29 -0300

    commit "final"

--- config/urls.py
+++ config/urls.py
@@ -5,8 +5,7 @@ from .import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('' ,include('config.urls')),
+    path('' , include('meuapp.urls')),
     path('admin/', admin.site.urls),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static('media/', document_root='c:/')
--- meuapp/models.py
+++ meuapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=100)
+    conteudo = models.TextField(max_length=5000)
+    imagem = models.ImageField(blank=True)
+    
+    def __str__(self):
+        return self.titulo
